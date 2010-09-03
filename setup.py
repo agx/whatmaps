464122bdc49ac224fe3ed19c957489106af76a43
Guido Günther <agx@sigxcpu.org>
2010-09-03 20:45:21 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf-8 :
+
+from distutils.core import setup
+
+setup(name = "whatmaps",
+      author = 'Guido Günther',
+      author_email = 'agx@sigxcpu.org',
+      scripts = [ 'whatmaps' ]
+)
+
