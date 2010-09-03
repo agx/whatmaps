e4e112bde13d79d4648b7f366c726a64577fd83b
Guido Günther <agx@sigxcpu.org>
2010-09-03 21:50:42 +0200

    Hook into apt pipeline

--- setup.py
+++ setup.py
@@ -3,9 +3,24 @@
 
 from distutils.core import setup
 
+data_files = []
+
+try:
+    import lsb_release
+    if lsb_release.get_distro_information()['ID'] in [ 'Debian' ]:
+       data_files = [('../etc/apt/apt.conf.d/',
+                      ['apt/50whatmaps_apt']),
+                     ('../etc/apt/apt.conf.d/',
+                      ['apt/20services']),
+                    ]
+except ImportError:
+    pass
+
 setup(name = "whatmaps",
       author = 'Guido Günther',
       author_email = 'agx@sigxcpu.org',
-      scripts = [ 'whatmaps' ]
+      data_files = data_files,
+      scripts = [ 'whatmaps' ],
 )
 
+# vim:et:ts=4:sw=4:et:sts=4:ai:set list listchars=tab\:»·,trail\:·:
