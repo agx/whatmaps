e34b48e2376ff19a598f48cdc42c5053f2e77193
Guido Günther <agx@sigxcpu.org>
2016-09-23 12:05:20 +0200

    setup.py: flake8 clean

--- setup.py
+++ setup.py
@@ -15,30 +15,32 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 from setuptools import setup
 
 data_files = []
 
 try:
     import lsb_release
-    if (lsb_release.get_distro_information()['ID'] in [ 'Debian' ] or
-        os.path.exists('/etc/debian_version')):
-       data_files = [('../etc/apt/apt.conf.d/',
-                      ['apt/50whatmaps_apt']),
-                     ('../etc/apt/apt.conf.d/',
-                      ['apt/20services']),
-                    ]
+    if (lsb_release.get_distro_information()['ID'] in ['Debian'] or
+            os.path.exists('/etc/debian_version')):
+        data_files = [('../etc/apt/apt.conf.d/',
+                       ['apt/50whatmaps_apt']),
+                      ('../etc/apt/apt.conf.d/',
+                       ['apt/20services']),
+                      ]
 except ImportError:
     pass
 
-setup(name = "whatmaps",
-      author = 'Guido Günther',
-      author_email = 'agx@sigxcpu.org',
-      data_files = data_files,
-      packages = ['whatmaps'],
-      entry_points = {
-          'console_scripts': [ 'whatmaps = whatmaps.command:run' ],
+
+setup(name="whatmaps",
+      author='Guido Günther',
+      author_email='agx@sigxcpu.org',
+      data_files=data_files,
+      packages=['whatmaps'],
+      entry_points={
+          'console_scripts': ['whatmaps = whatmaps.command:run'],
       },
-)
+      )
 
 # vim:et:ts=4:sw=4:et:sts=4:ai:set list listchars=tab\:»·,trail\:·:
